b9777bd77ccc49cac7d27271531b2a9c64e15889
Brenda <brendamatias.sobral@gmail.com>
2019-08-05 00:32:58 -0300

    Iniciando projeto

---
+++ gulpfile.js
@@ -0,0 +1,40 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const browserSync = require('browser-sync').create();
+
+
+function compilaSass() {
+    return gulp
+    .src('style/scss/*.scss')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }))
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false
+    }))
+    .pipe(gulp.dest('style/'))
+    .pipe(browserSync.stream());
+}
+
+gulp.task('sass', compilaSass);
+
+function browser() {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    })
+}
+
+gulp.task('browser-sync', browser);
+
+function watch() {
+    gulp.watch('style/scss/*.scss', compilaSass);
+    gulp.watch('*.html').on('change', browserSync.reload);
+}
+
+gulp.task('watch', watch);
+
+gulp.task('default', gulp.parallel('watch', 'browser-sync'));
