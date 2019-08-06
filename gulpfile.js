b0f23e1062fb7cad6360bdcc45e58a60423b0b84
Brenda <brendamatias.sobral@gmail.com>
2019-08-06 11:24:25 -0300

    Subindo alterações

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,43 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const autoprefixer = require('gulp-autoprefixer');
-const browserSync = require('browser-sync').create();
-
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const browserSync = require("browser-sync").create();
 
 function compilaSass() {
-    return gulp
-    .src('style/scss/*.scss')
-    .pipe(sass({
-        outputStyle: 'compressed'
-    }))
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
+  return gulp
+    .src("style/scss/*.scss")
+    .pipe(
+      sass({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"],
         cascade: false
-    }))
-    .pipe(gulp.dest('style/'))
+      })
+    )
+    .pipe(gulp.dest("style/"))
     .pipe(browserSync.stream());
 }
 
-gulp.task('sass', compilaSass);
+gulp.task("sass", compilaSass);
 
 function browser() {
-    browserSync.init({
-        server: {
-            baseDir: "./"
-        }
-    })
+  browserSync.init({
+    server: {
+      baseDir: "./"
+    }
+  });
 }
 
-gulp.task('browser-sync', browser);
+gulp.task("browser-sync", browser);
 
 function watch() {
-    gulp.watch('style/scss/*.scss', compilaSass);
-    gulp.watch('*.html').on('change', browserSync.reload);
+  gulp.watch("style/scss/*.scss", compilaSass);
+  gulp.watch(["*.html", "*.json"]).on("change", browserSync.reload);
 }
 
-gulp.task('watch', watch);
+gulp.task("watch", watch);
 
-gulp.task('default', gulp.parallel('watch', 'browser-sync'));
+gulp.task("default", gulp.parallel("watch", "browser-sync"));
